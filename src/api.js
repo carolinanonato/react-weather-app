db023919925b6da9bcbcc21a3046d4cd88c09f92
Carolina Nonato <csnonato@gmail.com>
2022-07-18 21:09:48 -0400

    search component

---
+++ src/api.js
@@ -0,0 +1,10 @@
+export const geoApiOptions = {
+    method: "GET",
+    headers: {
+        "X-RapidAPI-Key": "c94bdc0f9bmshbc7236941db8b8dp1307a5jsn3c7dc13612af",
+        "X-RapidAPI-Host": "wft-geo-db.p.rapidapi.com",
+    },
+};
+export const GEO_API_URL = "https://wft-geo-db.p.rapidapi.com/v1/geo";
+
+export const WEATHER_API_URL = "https://api.openweathermap.org/data/2.5";
